c006a8883fc28f8d1b6c2267333ad2ac9b3f31aa
David R. Piegdon <dgit@piegdon.de>
2019-03-05 23:19:03 +0000

    more cleanup, bughunting

--- top.v
+++ top.v
@@ -2,12 +2,12 @@
 
 module top(input wire CLK, output wire J1_8, output wire J1_9, output wire J1_10);
 
-	localparam LEDS=40;
+	localparam LEDS=32;
 	// NOTE: INTERPOLATIONS better be a power-of-two.
 	localparam INTERPOLATIONS=8;
-	// NOTE the +1 at the end of MILESTONES: avoids index-error for milestone_color_next.
+	// NOTE the +1 at the end of MILESTONES: avoids index-error for milestone_color_prev.
 	localparam MILESTONES=$rtoi($ceil((LEDS*1.0)/INTERPOLATIONS)) + 1;
-	localparam COLORBITS=8*3*MILESTONES;
+	localparam COLORBITS=MILESTONES*8*3;
 
 	localparam HOLDOFF_MAX=1200000;
 
@@ -21,7 +21,7 @@ module top(input wire CLK, output wire J1_8, output wire J1_9, output wire J1_10
 	reg [$clog2(INTERPOLATIONS-1):0] start_interpolation = 0;
 
 	reg [$clog2(LEDS-1):0] current_led = 0;
-	reg [$clog2(MILESTONES-1):0] current_milestone = 0;
+	reg [$clog2(MILESTONES-1):0] forward_milestone = 0;
 	reg [$clog2(INTERPOLATIONS-1):0] current_interpolation = 0;
 	reg [$clog2(3-1):0] current_rgb = 0;
 
@@ -30,11 +30,13 @@ module top(input wire CLK, output wire J1_8, output wire J1_9, output wire J1_10
 	wire [7:0] color_now_postgamma;
 	ws2812_output ws2812(CLK, rst, trigger, color_now_postgamma, trigger, data_request, J1_10);
 
-	wire [$clog2(COLORBITS-1):0] index_prev = current_milestone * 8*3 + current_rgb * 8;
-	wire [$clog2(COLORBITS-1):0] index_next = index_prev + 8*3;
-	wire [7:0] milestone_color_prev = colors[ index_prev+7 : index_prev ];
+	wire [$clog2(COLORBITS-1):0] index_next = forward_milestone * 8*3 + current_rgb * 8;
+	wire [$clog2(COLORBITS-1):0] index_prev = index_next + 8*3;
 	wire [7:0] milestone_color_next = colors[ index_next+7 : index_next ];
-	wire [7:0] color_now = (milestone_color_next*current_interpolation + milestone_color_prev*(INTERPOLATIONS-current_interpolation)) / INTERPOLATIONS;
+	wire [7:0] milestone_color_prev = colors[ index_prev+7 : index_prev ];
+	wire [7:0] color_now =  (  milestone_color_next*(INTERPOLATIONS-current_interpolation)
+				 + milestone_color_prev*current_interpolation
+				) / INTERPOLATIONS;
 	gammasight gammasight(color_now, color_now_postgamma);
 
 	wire [15:0] random;
@@ -51,42 +53,58 @@ module top(input wire CLK, output wire J1_8, output wire J1_9, output wire J1_10
 			colors <= 0;
 			start_interpolation <= 0;
 			current_led <= 0;
-			current_milestone <= 0;
+			forward_milestone <= 0;
 			current_interpolation <= 0;
 			current_rgb <= 0;
+		end
+
+		if(holdoff) begin
+			// timeout in between transmissions to WS2812 strip
+			holdoff <= holdoff-1;
 		end else if(data_request) begin
-			if(holdoff) begin
-				holdoff <= holdoff-1;
-			end else if(2 > current_rgb) begin
+			// WS2812 block gets an 8 bit segment for transmission
+			// this clock. so lets get the one for the next
+			// transmission ready.
+			if(2 > current_rgb) begin
+				// next 8bit R/G/B block within a single LED?
 				current_rgb <= current_rgb+1;
 			end else begin
 				current_rgb <= 0;
 				if(LEDS-1 > current_led) begin
+					// next led is ...
 					current_led <= current_led + 1;
 					if(INTERPOLATIONS-1 > current_interpolation) begin
+						// ... next interpolation between milestones
 						current_interpolation <= current_interpolation+1;
 					end else begin
+						// ... next milestone color
 						current_interpolation <= 0;
-						current_milestone <= current_milestone+1;
+						forward_milestone <= forward_milestone+1;
 					end
 				end else begin
+					// transmission of full strip completed. start a
+					// holdoff and prepare the next transmission: move
+					// the interpolations of milestones one slot along.
 					holdoff <= HOLDOFF_MAX;
 					current_led <= 0;
-					current_milestone <= 0;
-					if(INTERPOLATIONS-1 > start_interpolation) begin
-						start_interpolation <= start_interpolation+1;
-						current_interpolation <= start_interpolation+1;
+					forward_milestone <= 0;
+					if(0 != start_interpolation) begin
+						// only add a new interpolation
+						start_interpolation <= start_interpolation-1;
+						current_interpolation <= start_interpolation-1;
 					end else begin
-						start_interpolation <= 0;
-						current_interpolation <= 0;
-						colors <= { {random[14:10], 3'b0, random[9:5], 3'b0, random[4:0], 3'b0},
-								colors[COLORBITS-1 : 8*3]};
+						// insert a new random milestone color
+						// and start interpolating towards it.
+						start_interpolation <= INTERPOLATIONS-1;
+						current_interpolation <= INTERPOLATIONS-1;
+						colors <= { 
+								colors[COLORBITS-1-8*3 : 0],
+								random[14:10], 3'b0,
+								random[9:5], 3'b0,
+								random[4:0], 3'b0
+							};
 					end
 				end
-			end
-		end else begin
-			if(holdoff) begin
-				holdoff <= holdoff-1;
 			end
 		end
 	end
