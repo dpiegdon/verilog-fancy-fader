5fba1ab1a1ffe6b0e7d7ffca3db30c650d952e57
David R. Piegdon <dgit@piegdon.de>
2019-03-22 00:55:44 +0000

    added/improved testbench for fancy fader

--- ws2812_fancy_fader_tb.v
+++ ws2812_fancy_fader_tb.v
@@ -12,20 +12,37 @@ module ws2812_fancy_fader_tb();
 	wire trigger;
 	wire [7:0] color_now;
 	
-	ws2812_fancy_fader dut(clk[0], rst, random, data_request, trigger, color_now);
+	ws2812_fancy_fader #(.HOLDOFF_TIME(4)) dut(clk[0], rst, random, data_request, trigger, color_now);
 
 	always #1 clk = clk+1;
+
+	integer k;
+	integer i;
 
 	initial begin
 		$dumpfile("ws2812_fancy_fader_tb.vcd");
 		$dumpvars(0, ws2812_fancy_fader_tb);
 
 		clk = 0;
-		rst = 0;
-		random = 0;
+		rst = 1;
+		random = 16'haaaa;
 		data_request = 0;
 
-		repeat(100) @(posedge clk);
+		repeat(2) @(negedge clk);
+
+		rst = 0;
+
+		repeat(2) @(negedge clk);
+
+		for(i=0; i<4096; i=i+1) begin
+			data_request = 1;
+			repeat(1) @(negedge clk);
+
+			data_request = 0;
+			repeat(3) @(negedge clk);
+		end
+
+		repeat(8) @(negedge clk);
 
 		$finish;
 	end
